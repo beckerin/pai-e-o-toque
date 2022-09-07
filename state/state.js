aa64180799d2a91bb3bde38c4103313fbb09398b
Diego Paiva <paivadiego7797@gmail.com>
2022-09-07 17:19:11 +0100

    Change score base

--- state/state.js
+++ state/state.js
@@ -1,48 +1,47 @@
 export default function createState() {
-    const screen = {
-        height: 200,
-        width: 250,
+  const screen = {
+    height: 500,
+    width: 900,
+  };
+  const observers = [];
+  const difficult = {
+    speed: 0.05,
+    spawn: 0.05,
+    size: 20,
+  };
+
+  const textRenders = {};
+
+  const currentPlayer = null;
+  const players = {};
+  const obstacles = {};
+
+  const points = {
+    max: 9999,
+    playerId: "diego",
+  };
+
+  function reset() {
+    for (const obstacleId in obstacles) {
+      delete obstacles[obstacleId];
     }
-    const observers = []
-    const difficult = {
-        speed: 0.1,
-        spawn: 0.5,
-        size: 10
+    for (const playerId in players) {
+      delete players[playerId];
     }
+  }
+  return {
+    screen,
+    observers,
+    difficult,
 
-    const textRenders = {}
+    textRenders,
 
-    const currentPlayer = null
-    const players = {}
-    const obstacles = {}
+    currentPlayer,
+    players,
+    obstacles,
 
-    const points = {
-        max: 1,
-        playerId: 'diego'
-    }
+    points,
 
-
-    function reset() {
-        for (const obstacleId in obstacles) {
-            delete obstacles[obstacleId]
-        }
-        for (const playerId in players) {
-            delete players[playerId]
-        }
-    }
-    return  {
-        screen,
-        observers,
-        difficult,
-
-        textRenders,
-
-        currentPlayer,
-        players,
-        obstacles,
-
-        points,
-
-        reset,
-    }
+    reset,
+  };
 }
